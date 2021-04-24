e8490dcc0f2978f227b3adc8a25c1da85dde34cd
desirel <two_somang@icloud.com>
2021-04-25 03:35:24 +0900

    chaged message

--- src/main/java/com/example/cpuboundapplication/HashController.java
+++ src/main/java/com/example/cpuboundapplication/HashController.java
@@ -29,9 +29,9 @@ public class HashController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello deploy";
     }
-
+    
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("MD5");
         md.update(input.getBytes());
